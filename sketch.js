b9ec45f5ab37b42109badb86bcfb039115e7f1b4
AnchuriS <105028352+AnchuriS@users.noreply.github.com>
2022-05-12 18:46:26 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,50 +1,45 @@
-var trex, trex_running, edges;
-var groundImage;
-var ground;
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
 
-function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  groundImage = loadImage("ground2.png")
+function preload() {
+    trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+    trex_collided = loadImage("trex_collided.png");
+    groundImage = loadImage("ground2.png")
+}
+function setup() {
+    createCanvas(600, 200);
+
+    //create a trex sprite
+    trex = createSprite(50,160,20,50);
+    trex.addAnimation("running", trex_running);
+    trex.scale = 0.5;
+
+    //create a ground sprite
+    ground = createSprite(200,180,400,20);
+    ground.addImage("ground",groundImage);
+    ground.x = ground.width /2;
+    ground.velocityX = -4;
+
+    invisibleGround=createSprite(200,195,400,20);
+    invisibleGround.visible=false
 }
 
-function setup(){
-  createCanvas(600,200);
+function draw() {
+    background(220);
 
-  // creating trex
-  trex = createSprite(50,160,20,50);
-  trex.addAnimation("running", trex_running);
-  edges = createEdgeSprites();
-  
-  //adding scale and position to trex
-  trex.scale = 0.5;
-  trex.x = 50
+    //jump when the space button is pressed
+    if (keyDown("space")&& trex.y>100) {
+        trex.velocityY = -10;
+    }
 
-  ground= createSprite(300,170,600,5);
-  ground.addImage("ground",groundImage);
-  ground.velocityX=-3;
-}
+    trex.velocityY = trex.velocityY + 0.8
 
+    if (ground.x < 0) {
+        ground.x = ground.width / 2;
+    }
 
-function draw(){
-  //set background color 
-  background(190);
-  
-  //logging the y position of the trex
-  console.log(trex.y)
-  
-  //jump when space key is pressed
-  if(keyDown("space") && trex.y>100){
-    trex.velocityY = -10;
-  }
-  
-  trex.velocityY = trex.velocityY + 0.5;
-  
-  //stop trex from falling down
-  trex.collide(ground)
-  //console.log(ground.x);
+    trex.collide(invisibleGround);
 
-  if(ground.x<0){
-    ground.x=ground.width/2;
-  }
-  drawSprites();
+   
+    drawSprites();
 }
